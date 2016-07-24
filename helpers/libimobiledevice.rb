c468425f310d5c02f040b3e452b745fb79d8ab56
Jing Li <jingli@ebay.com>
2016-07-25 01:05:48 +0200

    Change LibImobileDevice from module to class

--- helpers/libimobiledevice.rb
+++ helpers/libimobiledevice.rb
@@ -1,18 +1,20 @@
 module MobileDevicePool
-  module LibImobileDevice
-    def list_devices
-      `idevice_id --list`.split("\n")
-    end
-    
-    def show_battery_level(udid)
-      get_info('ideviceinfo', udid, 'BatteryCurrentCapacity', 'com.apple.mobile.battery')
-    end
-    
-    def get_info(cmd, udid, key, domain = nil)
-      if domain
-        `#{cmd} -u #{udid} -q #{domain} -k #{key}`.strip
-      else
-        `#{cmd} -u #{udid} -k #{key}`.strip
+  class LibImobileDevice
+    class << self
+      def list_devices
+        `idevice_id --list`.split("\n")
+      end
+      
+      def show_battery_level(udid)
+        get_info('ideviceinfo', udid, 'BatteryCurrentCapacity', 'com.apple.mobile.battery')
+      end
+      
+      def get_info(cmd, udid, key, domain = nil)
+        if domain
+          `#{cmd} -u #{udid} -q #{domain} -k #{key}`.strip
+        else
+          `#{cmd} -u #{udid} -k #{key}`.strip
+        end
       end
     end
   end

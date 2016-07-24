a6cc79644d5d39a6c39a8b2f33dcd7d346fcd156
Jing Li <jingli@ebay.com>
2016-07-25 01:29:35 +0200

    Add more info getters to LibImobileDevice

--- helpers/libimobiledevice.rb
+++ helpers/libimobiledevice.rb
@@ -1,11 +1,41 @@
 module MobileDevicePool
   class LibImobileDevice
+    @@product_type_name_map = File.open(File.join(File.dirname(__FILE__), 'ios_product_type_name_map'), 'r') do |f|
+      list = {}
+      while line = f.gets
+        if line.match(/^#.*/).nil?
+          map = line.strip.split('=')
+          list[map.first] = map.last
+        end
+      end
+      list
+    end
+    
     class << self
       def list_devices
         `idevice_id --list`.split("\n")
       end
       
-      def show_battery_level(udid)
+      def list_devices_with_details
+        devices = list_devices.inject([]) do |devices, udid|
+          device = {}
+          device['udid'] = udid
+          device['model'] = get_product_name(udid)
+          device['os'] = get_os_version(udid)
+          device['battery'] = get_battery_level(udid)
+          devices.push(device)
+        end
+      end
+      
+      def get_os_version(udid)
+        get_info('ideviceinfo', udid, 'ProductVersion')
+      end
+      
+      def get_product_name(udid)
+        @@product_type_name_map[get_info('ideviceinfo', udid, 'ProductType')]
+      end
+      
+      def get_battery_level(udid)
         get_info('ideviceinfo', udid, 'BatteryCurrentCapacity', 'com.apple.mobile.battery')
       end
       
@@ -17,5 +47,7 @@ module MobileDevicePool
         end
       end
     end
+    
+    private_class_method :get_info
   end
 end
